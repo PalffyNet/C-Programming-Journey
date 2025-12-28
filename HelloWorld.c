74fe04746d2de8aee3c5c8ae482a3ff756ad9fd0
PalffyNet <132748435+PalffyNet@users.noreply.github.com>
2025-12-28 09:56:21 -0500

    Update and rename HelloWorld to HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,9 @@
+//This is the quintessential entry point into the C-Programming language. It is also, good to run as a test for any compiler or troubleshooting. Nothing original or special about this.
+
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("hello, world!\n");
+    return 0;
+}
